89b3c040cd6014ad42a27bfcf8887ffaa8a3f7ab
Martin Gothe <martingothe8@gmail.com>
2023-10-04 21:44:20 -0300

    Preentrega3

--- script.js
+++ script.js
@@ -1,103 +1,120 @@
-let nombre = "Juan Gomez"
-let contraseña1 = "Talleres1"
+let productos = [
+    { id: 1, nombre: "ARGENTINA 1986", categoria: "remera", stock: 2, precio: (35000), rutaImagen: "argentina-1986.jpg"},
+    { id: 2, nombre: "BRASIL 1970", categoria: "remera", stock: 5, precio: (25000), rutaImagen: "brasil-1970.jpg"},
+    { id: 3, nombre: "ALEMANIA 1972", categoria: "remera", stock: 1, precio: (25000), rutaImagen: "deutschland-1972.jpg"},
+    { id: 4, nombre: "HOLANDA 1978", categoria: "remera", stock: 4, precio: (25000), rutaImagen: "nederland-1978.jpg"},
+]
 
-console.log (nombre)
-
-let usuario
-let contraseña
-let contador = 0
-let inicioSecion = false
-
-do{
-    usuario = prompt("Ingrese su usuario")
-    contraseña = prompt("Ingrese su contraseña")
-    contador ++
-    if (usuario === nombre && contraseña === contraseña1) {
-        alert("Bienvenido " + usuario)
-        inicioSecion = true
-        break
-    } else {
-        alert("Usuario y/o contraseña incorrecto/s")
-    }
-} while (contador < 3)
-    if (contador === 3 && !inicioSecion) {
-        alert("Agotó su número de intentos posibles")
-    }
-
-
-function Mayusculas(texto) {
-        return texto.toUpperCase()
-}
-let usuarioMayuscula = Mayusculas(usuario)
-console.log(usuarioMayuscula)
-
-let edad = prompt("Ingrese su edad")
-if (edad >= 18) {
-    alert("Tiene edad para ingresar al sitio")
-}
-else {
-    alert("No tiene edad para ingresar al sitio, solicitamos la presencia de un adulto")
-}
-
-function itemsComprados(arrayDeElemntos, funcion) {
-    for (const elemento of arrayDeElemntos) {
-        alert(`${elemento.cantidad} ${elemento.tipo}`)
-    }
-}
 let carrito = []
-while (true) {
-    let opcionCompra = prompt("Ingrese un número: '1' para comprar Entradas de Infante (0-5 años), '2' para entradas de Adolescente (6-21 años), '3' para entrada de adulto +21")
+let carritoRecuperado = localStorage.getItem("carrito")
+if (carritoRecuperado) {
+    carrito= JSON.parse(carritoRecuperado)
+}
 
-    switch (opcionCompra) {
-        case "1":
-            alert("Infante")
-            carrito.push({ tipo: "Infante", precio: 5 })
-            break
-        case "2":
-            alert("Adolescente")
-            carrito.push({ tipo: "Adolescente", precio: 10 })
-            break
-        case "3":
-            alert("Adulto")
-            carrito.push({ tipo: "Adulto", precio: 15 })
-            break
-        default:
-            alert("Opción incorrecta")
-            continue;
+renderizarProductos(productos)
+renderizarCarrito(carrito)
+
+function renderizarProductos(productos) {
+    let contenedor = document.getElementById("contenedorProductos")
+    contenedor.innerHTML = ""
+
+    productos.forEach(producto => {
+        let tarjeta = document.createElement("div")
+        tarjeta.className = "tarjeta"
+
+        tarjeta.innerHTML = `
+        <h3 class="nombreProducto">${producto.nombre}</h3>
+        <img src="./assets/img/${producto.rutaImagen}" />
+        <p class="precio">$${producto.precio}</p>
+        <button id="${producto.id}">Agregar al carrito</button>
+        `
+
+        contenedor.appendChild(tarjeta)
+
+        let botonAgregarAlCarrito = document.getElementById(producto.id)
+        botonAgregarAlCarrito.addEventListener("click", (e) => agregarProductoAlCarrito(productos, carrito, e))
+    })
+}
+
+let buscador = document.getElementById("buscador")
+let botonBuscar = document.getElementById("buscar")
+let resultado = document.getElementById("resultado")
+
+botonBuscar.addEventListener("click", () => filtrarYRenderizar(productos))
+buscador.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        filtrarYRenderizar(productos)
     }
+})
 
-    let cantidad = Number(prompt("Ingrese la cantidad que desea comprar:"))
-    carrito[carrito.length - 1].cantidad = cantidad
+function filtrarYRenderizar(productos){
+    let textoBusqueda = buscador.value.toUpperCase()
+    let productosFiltrados = productos.filter(producto => producto.nombre.toUpperCase().includes(textoBusqueda))
+    renderizarProductos(productosFiltrados)
 
-    let continuar = prompt("¿Desea agregar otro objeto? (Sí: '1', No: '0')").toLowerCase()
-    if (continuar !== '1') {
-        let total = 0
-
-        //alert("Resumen de la compra:")//
-    for (const item of carrito) {
-      console.log(`${item.cantidad}x Entrada ${item.tipo} - $${item.precio * item.cantidad}`)
-      total += item.precio * item.cantidad
-    }
-
-    alert(`Total: $${total}`)
-
-    itemsComprados(carrito, alert)
-    break
+    if (productosFiltrados.length > 0) {
+        renderizarProductos(productosFiltrados)
+        resultado.textContent = ""
+    } else {
+        resultado.textContent = "El producto solicitado no existe"
     }
 }
 
-function verificarLongitud(numero, longitudEsperada) {
-    return numero.length === longitudEsperada
+function agregarProductoAlCarrito(productos, carrito, e) {
+    let productoBuscado = productos.find( producto => producto.id === Number(e.target.id))
+    let productoEnCarrito = carrito.find( producto => producto.id === productoBuscado.id)
+
+    if (productoBuscado.stock > 0) {
+        if (productoEnCarrito) {
+            productoEnCarrito.unidades++
+            productoEnCarrito.subtotal = productoEnCarrito.unidades * productoEnCarrito.precioUnitario
+        } else {
+            carrito.push({
+                id: productoBuscado.id,
+                nombre: productoBuscado.nombre,
+                precioUnitario: productoBuscado.precio,
+                unidades: 1,
+                subtotal:productoBuscado.precio,
+            })
+        }
+        productoBuscado.stock --
+        localStorage.setItem("carrito", JSON.stringify(carrito))
+        alert("Se agregó el producto al carrito")
+        } else{
+            alert("No hay stock del producto selecionado")
+        }
+
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+
+        renderizarCarrito(carrito)
+    }
+
+function renderizarCarrito(productosEnCarrito) {
+    let divCarrito = document.getElementById("carrito")
+    divCarrito.innerHTML= ""
+
+    productosEnCarrito.forEach(producto => {
+        let tarjetaProductoCarrito = document.createElement("div")
+        tarjetaProductoCarrito.innerHTML= `
+        <p>${producto.nombre}</p>
+        <img src="./assets/img/${producto.rutaImagen}" />
+        <p>Precio: $${producto.precioUnitario}</p>
+        <p>Unidades: ${producto.unidades}</p>
+        <p>Subtotal: $${producto.subtotal}</p>
+        `
+
+        divCarrito.appendChild(tarjetaProductoCarrito)
+    })
 }
 
-let numeroTarjeta = prompt("Por favor ingresa un número de tarjeta:")
-let numeroCodigo = prompt("Por favor ingresa el código de seguridad de la tarjeta:")
-let longitudEsperadaTarjeta = 16
-let longitudEsperadaCodigo = 3
+let botonVerOcultar = document.getElementById("verOcultar")
+botonVerOcultar.addEventListener("click", verOcultarCarrito)
 
-if ((verificarLongitud(numeroTarjeta, longitudEsperadaTarjeta)) && (verificarLongitud(numeroCodigo, longitudEsperadaCodigo))) {
-    alert("Su compra ha sido efectuada." )
-    
-} else {
-    alert("El número de tarjeta y/o su código de seguridad son incorrectos.")
+function verOcultarCarrito() {
+    let carrito = document.getElementById("carrito")
+    let contenedorProductos = document.getElementById("contenedorProductos")
+
+    carrito.classList.toggle("oculta")
+    contenedorProductos.classList.toggle("oculta")
 }
+
